67d56eb4281add3e52637d5d1bbdb596cda84973
Danielle Sucher <dsucher@gmail.com>
2013-06-18 01:31:14 -0400

    Save name for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :goal_increment, :oauth_token, :oauth_token_secret, :provider, :uid
+  attr_accessible :email, :goal_increment, :name, :oauth_token, :oauth_token_secret, :provider, :uid
 
   def self.from_omniauth(auth)
     if user = User.find_by_uid(auth.uid)
+      user.name = auth.info.display_name
       user.oauth_token = auth.extra.access_token.params['oauth_token']
       user.oauth_token_secret = auth.extra.access_token.params['oauth_token_secret']
       user.save!
+      user
     else
       where(auth.slice(:provider, :uid)).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
+        user.name = auth.info.display_name
         user.oauth_token = auth.extra.access_token.params['oauth_token']
         user.oauth_token_secret = auth.extra.access_token.params['oauth_token_secret']
+        user.provider = auth.provider
+        user.uid = auth.uid
       end
     end
   end
