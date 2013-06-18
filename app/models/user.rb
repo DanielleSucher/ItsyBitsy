7227e09117086b9ec470007c7aad88903a518c5f
Danielle Sucher <dsucher@gmail.com>
2013-06-18 01:19:05 -0400

    Update existing users' tokens/secrets as needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ActiveRecord::Base
   attr_accessible :email, :goal_increment, :oauth_token, :oauth_token_secret, :provider, :uid
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
+    if user = User.find_by_uid(auth.uid)
       user.oauth_token = auth.extra.access_token.params['oauth_token']
       user.oauth_token_secret = auth.extra.access_token.params['oauth_token_secret']
+      user.save!
+    else
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.oauth_token = auth.extra.access_token.params['oauth_token']
+        user.oauth_token_secret = auth.extra.access_token.params['oauth_token_secret']
+      end
     end
   end
 
