9a049b3f6434e0e545c1d5947e79f6b27b5892ea
Danielle Sucher <dsucher@gmail.com>
2013-06-18 01:37:37 -0400

    Simplify User::from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,11 @@ class User < ActiveRecord::Base
   attr_accessible :email, :goal_increment, :name, :oauth_token, :oauth_token_secret, :provider, :uid
 
   def self.from_omniauth(auth)
-    if user = User.find_by_uid(auth.uid)
-      user.name = auth.info.display_name
-      user.oauth_token = auth.extra.access_token.params['oauth_token']
-      user.oauth_token_secret = auth.extra.access_token.params['oauth_token_secret']
-      user.save!
-      user
-    else
-      where(auth.slice(:provider, :uid)).first_or_create do |user|
-        user.name = auth.info.display_name
-        user.oauth_token = auth.extra.access_token.params['oauth_token']
-        user.oauth_token_secret = auth.extra.access_token.params['oauth_token_secret']
-        user.provider = auth.provider
-        user.uid = auth.uid
-      end
-    end
+    user = User.where(auth.slice(:provider, :uid)).first_or_create
+    user.update_attributes name: auth.info.display_name,
+                           oauth_token: auth.extra.access_token.params['oauth_token'],
+                           oauth_token_secret: auth.extra.access_token.params['oauth_token_secret']
+    user
   end
 
   def fitbit_goal
